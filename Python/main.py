5ad9ee0228a38ead0ec6d06f60fdc722366b3e9f
Jason Kim <dmdwn2696@naver.com>
2019-07-20 18:06:46 -0400

    Add logic to terminate loop upon button retrigger

--- Python/main.py
+++ Python/main.py
@@ -3,6 +3,7 @@ import keyboard
 import threading
 import sys
 import mido
+import timeit
 
 CHUNK = 1024
 FORMAT = pyaudio.paInt16
@@ -55,14 +56,18 @@ class AudioInput:
                     input_device_index=3,
                     frames_per_buffer=CHUNK)
         self.frames = []
-        self.startPlaying = False
+        self.startRecording = False
+        self.recordFinished = False
+        self.shouldTerminate = False
 
-    def record_and_play(self):
+    def record_and_play(self, index):
         while True:
+            print ('Recording for Looper Thread %s' % index)
+
             data = self.stream.read(CHUNK, exception_on_overflow=False)
             self.frames.append(data)
 
-            if not self.startPlaying:
+            if not self.startRecording:
                 self.stream.stop_stream()
                 self.stream.close()
                 self.p.terminate()
@@ -73,6 +78,9 @@ class AudioInput:
 
     def loop_play(self):
         while True:
+            if self.shouldTerminate:
+                break
+
             a = AudioOutput()
             a.play(b''.join(self.frames))
 
@@ -83,10 +91,10 @@ if __name__ == "__main__":
     audioInput3 = AudioInput()
     audioInput4 = AudioInput()
 
-    my_thread1 = threading.Thread(target=audioInput1.record_and_play)
-    my_thread2 = threading.Thread(target=audioInput2.record_and_play)
-    my_thread3 = threading.Thread(target=audioInput3.record_and_play)
-    my_thread4 = threading.Thread(target=audioInput4.record_and_play)
+    my_thread1 = threading.Thread(target=audioInput1.record_and_play, args=("1",))
+    my_thread2 = threading.Thread(target=audioInput2.record_and_play, args=("2",))
+    my_thread3 = threading.Thread(target=audioInput3.record_and_play, args=("3",))
+    my_thread4 = threading.Thread(target=audioInput4.record_and_play, args=("4",))
 
     try:
         with mido.open_input(portname) as port:
@@ -96,29 +104,45 @@ if __name__ == "__main__":
             for message in port:
                 msgDict = message.dict()
 
-                if msgDict['type'] == 'note_on' and msgDict['note'] == 60:
-                    audioInput1.startPlaying = True
+                if msgDict['type'] == 'note_on' and msgDict['note'] == 60 and not audioInput1.recordFinished:
+                    audioInput1.startRecording = True
                     my_thread1.start()
-                elif msgDict['type'] == 'note_off' and msgDict['note'] == 60:
-                    audioInput1.startPlaying = False
+                elif msgDict['type'] == 'note_off' and msgDict['note'] == 60 and not audioInput1.recordFinished:
+                    audioInput1.startRecording = False
+                    audioInput1.recordFinished = True
+                elif msgDict['type'] == 'note_on' and msgDict['note'] == 60 and audioInput1.recordFinished:
+                    audioInput1.shouldTerminate = True
+                    audioInput1.recordFinished = False
 
-                if msgDict['type'] == 'note_on' and msgDict['note'] == 61:
-                    audioInput2.startPlaying = True
+                if msgDict['type'] == 'note_on' and msgDict['note'] == 61 and not audioInput2.recordFinished:
+                    audioInput2.startRecording = True
                     my_thread2.start()
-                elif msgDict['type'] == 'note_off' and msgDict['note'] == 61:
-                    audioInput2.startPlaying = False
+                elif msgDict['type'] == 'note_off' and msgDict['note'] == 61 and not audioInput2.recordFinished:
+                    audioInput2.startRecording = False
+                    audioInput2.recordFinished = True
+                elif msgDict['type'] == 'note_on' and msgDict['note'] == 61 and audioInput2.recordFinished:
+                    audioInput2.shouldTerminate = True
+                    audioInput2.recordFinished = False
 
-                if msgDict['type'] == 'note_on' and msgDict['note'] == 62:
-                    audioInput3.startPlaying = True
+                if msgDict['type'] == 'note_on' and msgDict['note'] == 62 and not audioInput3.recordFinished:
+                    audioInput3.startRecording = True
                     my_thread3.start()
-                elif msgDict['type'] == 'note_off' and msgDict['note'] == 62:
-                    audioInput3.startPlaying = False
+                elif msgDict['type'] == 'note_off' and msgDict['note'] == 62 and not audioInput3.recordFinished:
+                    audioInput3.startRecording = False
+                    audioInput3.recordFinished = True
+                elif msgDict['type'] == 'note_on' and msgDict['note'] == 62 and audioInput3.recordFinished:
+                    audioInput3.shouldTerminate = True
+                    audioInput3.recordFinished = False
 
-                if msgDict['type'] == 'note_on' and msgDict['note'] == 63:
-                    audioInput4.startPlaying = True
+                if msgDict['type'] == 'note_on' and msgDict['note'] == 63 and not audioInput4.recordFinished:
+                    audioInput4.startRecording = True
                     my_thread4.start()
-                elif msgDict['type'] == 'note_off' and msgDict['note'] == 63:
-                    audioInput4.startPlaying = False
+                elif msgDict['type'] == 'note_off' and msgDict['note'] == 63 and not audioInput4.recordFinished:
+                    audioInput4.startRecording = False
+                    audioInput4.recordFinished = True
+                elif msgDict['type'] == 'note_on' and msgDict['note'] == 63 and audioInput4.recordFinished:
+                    audioInput4.shouldTerminate = True
+                    audioInput4.recordFinished = False
 
     except KeyboardInterrupt:
         pass
